359c30f87cab97eb4180c4918c4c287ed85c8ce4
Yasir Ahmad <khan10055yasir@gmail.com>
2022-01-24 16:35:21 +0000

    bot working with wordlist

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ import os
 import discord 
 import requests
 import json
+import random
 
 
 
 client = discord.Client()
 my_secret = os.environ['discord_key']
 
+sad_words = ['sad',"depressed",'unhappy','angry']
+
+starter_encouragements = [
+  'cheer up', 'hang in there!',
+  'You are a great person/ Bot',
+  'Tesnion not Bae!'
+] 
+
 def getquote():
-  response = requests.get('https://zenquotes.io/api/random') 
-  json_data = json.load(response.text)
+  response = requests.get('https://zenquotes.io/api/random')
+  json_data = json.loads(response.text)
   quote = json_data[0]['q'] + ' -'+ json_data[0]['a'] 
-  print(quote)
+
   return(quote)
 
 @client.event
@@ -26,9 +35,13 @@ async def on_message(message):
   if message.author == client.user:
     return 
 
+  msg = message.content
+  
+  if message.content.startswith('sad'):
+    quote = getquote()
+    await message.channel.send(quote)
 
-  if message.content.startswith('$hi'):
-    await message.channel.send(getquote())
-
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragements))
 client.run(my_secret)
 
