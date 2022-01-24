6fdd646058eb99447619e7be36b89160357e5fc5
Yasir Ahmad <khan10055yasir@gmail.com>
2022-01-24 17:02:24 +0000

    working with db

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import discord
 import requests
 import json
 import random
-
+from replit import db
 
 
 client = discord.Client()
@@ -25,6 +25,22 @@ def getquote():
 
   return(quote)
 
+def update_encouragement(encouraging_message):
+  if "encouragements" in db.keys():
+    encouragments  = db['encouragments']
+    encouragments.append(encouraging_message)
+    db["encouragments"] = encouragments
+  else:
+    db[encouragments] = [encouraging_message]
+
+def delete_encouragement(index):
+  encouragements = db['encouragements']
+  if len(encouragements) > index:
+    del encouragements[index]
+    db['encouragements'] = encouragements
+
+
+
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
@@ -42,10 +58,31 @@ async def on_message(message):
     quote = getquote()
     await message.channel.send(quote)
 
+  options = starter_encouragements
+  if "encouragements" in db.keys():
+    options = options+ db['encouragements']
+
   elif any(word in msg for word in sad_words):
-    await message.channel.send(random.choice(starter_encouragements))
+    await message.channel.send(random.choice(options))
+
+  if msg.startswith('$new'):
+    encouraging_message = msg.split("$new ",1)[1]
+    update_encouragement(encouraging_message)
+    await message.channel.send("New encouraging message added")
+
+  if msg.startswith('$delete'):
+    encouragments = []
+    if 'encouragments' in db.keys():
+      index = int(msg.split("$delete", 1)[1])
+      delete_encouragement(index)
+      encouragments = db['encouragments']
+    await message.channel.send(encouragments)
+
+
 
   elif any(word in msg for word in wellcome_words):
     await message.channel.send('Your wellcome Sir :)')
+
+
 client.run(my_secret)
 
