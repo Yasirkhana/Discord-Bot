2e5a407e4bd5ff58e8c86949cfb55eac1ba3bff4
Yasir Ahmad <khan10055yasir@gmail.com>
2022-01-24 15:11:44 +0000

    Simple bot working

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import os
 import discord 
 
-client = discord.client()
+client = discord.Client()
 my_secret = os.environ['discord_key']
+
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format{client})
+  print('We have logged in as {0.user}'.format(client))
 
 
 @client.event
 async def on_message(message):
   if message.author == client.user:
-  
     return 
 
 
-  if message.content.startwith('$Hello'):
+  if message.content.startswith('$Hello'):
     await message.channel.send('Hello!')
 
-  client.run(my_secret)
+client.run(my_secret)
 
